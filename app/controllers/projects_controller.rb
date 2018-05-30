57b1817bc9c1a93f6306c7b26b4a11c42e224ec4
Jonas Preisler <jonas.preisler@gmail.com>
2018-05-30 13:34:59 +0800

    added website to projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,6 +70,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :image, :desciption, :service_id, :intro)
+      params.require(:project).permit(:name, :image, :desciption, :service_id, :intro, :website)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_145207) do
+ActiveRecord::Schema.define(version: 2018_05_30_053126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_05_29_145207) do
     t.datetime "updated_at", null: false
     t.integer "service_id"
     t.text "intro"
+    t.string "website"
   end
 
   create_table "services", force: :cascade do |t|
