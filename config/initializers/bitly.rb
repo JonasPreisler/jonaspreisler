4af67392d959d0449d91885adb33c2a5ea6a5d35
jonaspreisler <jonas.preisler@g>
2018-08-30 14:52:44 +0800

    changed login name

--- config/initializers/bitly.rb
+++ config/initializers/bitly.rb
@@ -1,5 +1,5 @@
 Bitly.configure do |config|
   config.api_version = 3
-  config.login = "JonasPreisler"
+  config.login = "jonaspreisler"
   config.api_key = "R_7d1898936b0e8cef8db454ab84738ea5"
 end
