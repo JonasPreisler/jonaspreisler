1b0087f58ad4e22e9409e2f5ae73b0acf3524873
Jonas <jonas.preisler@g>
2019-02-13 21:17:33 +0800

    Changed aws envs to capital letters

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['aws_access_key_id'],
-      :aws_secret_access_key => ENV['aws_secret_access_key'],
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
       :region                => ENV['S3_REGION']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
